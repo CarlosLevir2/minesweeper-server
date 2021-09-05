d2e75642c9bdb00311d638e660658cc06e16c1cd
Carlos Levir <carloslevirfs@gmail.com>
2021-09-05 19:41:23 -0300

    feat: set services up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import { QueryClientProvider } from "react-query";
+import Main from "./pages/Main";
+import { queryClient } from "./services/query-client";
 
 function App() {
   return (
-    <div>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Main />
+    </QueryClientProvider>
   );
 }
 
