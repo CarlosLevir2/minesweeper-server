29a190467a9f5da57c609ea5e97786c948215f6f
Carlos Levir <carloslevirfs@gmail.com>
2021-09-07 17:05:10 -0300

    feat: add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import routes from './api/api';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 8000);
