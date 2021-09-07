2edb2145ce1e7af4dbb4c17c7d52e66b29776e0d
Carlos Levir <carloslevirfs@gmail.com>
2021-09-07 17:05:24 -0300

    feat: creates back types

--- server/src/model/board/types.ts
+++ server/src/model/board/types.ts
@@ -1,15 +1,26 @@
+export type CellCoordinates = {
+  rowIndex: number;
+  cellIndex: number;
+};
+
 export type Cell = {
   hasMine?: boolean;
   hasFlag: boolean;
   rowIndex: number;
   cellIndex: number;
-  reveled: boolean;
+  closeMines?: number;
 };
 
-export type Board = Cell[][];
+export type Row = Cell[];
 
 export type Boards = {
-  [key: string]: Board;
+  [key: string]: {
+    id: string;
+    rows: Row[];
+    gameFinished: boolean;
+    win: boolean;
+    loose: boolean;
+  };
 };
 
 export type MineMap = {
