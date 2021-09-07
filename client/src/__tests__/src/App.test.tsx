9642e5f4c1a9e9fc5b03ceb23ab823f505b8512a
Carlos Levir <carloslevirfs@gmail.com>
2021-09-07 19:35:37 -0300

    feat: split packages

--- client/src/__tests__/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../../App';
-
-describe('APP', () => {
-  test('It should render App', () => {
-    const { container } = render(<App />);
-    expect(container).toMatchSnapshot();
-  });
-});
